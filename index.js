b9fa457f46d413602ceea1f34e60aef4227d5c6f
Akshay <98071270+AkKnight08@users.noreply.github.com>
2023-07-05 11:53:18 +0530

    / (Home) & /money Connected

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express= require('express');
 const port=8000;
 const app=express();
 
+app.get('/',function(req,res)
+{
+   res.end('<h1>Hi you are Connected to To Do List</h1>');
+});
+
+app.get('/money',function(req,res)
+{
+  res.end('<h1>Hi you are Connected to Money Management</h1>');
+});
+
 app.listen(port,function(err)
 {
   if(err)
