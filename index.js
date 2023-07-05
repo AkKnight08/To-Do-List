edffd5dbbf8155130dde1233b23a82962c266f49
Akshay <98071270+AkKnight08@users.noreply.github.com>
2023-07-05 12:05:15 +0530

    Ejs Template Assigned and app.set

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express= require('express');
+const path=require('path');
+const { dirname }=require('path');
 const port=8000;
 const app=express();
+
+// app.set('view engine','ejs');
+// app.set('views',path.join(__dirname,'views'));
 
 app.get('/',function(req,res)
 {
