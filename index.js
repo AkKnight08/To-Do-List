ee92b994910a5e9f47167f15b6c0ba86465ee4af
Akshay <98071270+AkKnight08@users.noreply.github.com>
2023-07-05 12:43:08 +0530

    Body Parser Setup done, home page setup done, Ready to add assets

--- index.js
+++ index.js
@@ -1,31 +1,59 @@
-const express= require('express');
-const path=require('path');
-const { dirname }=require('path');
-const port=8000;
-const app=express();
+const express = require('express');
+const path = require('path');
+const { dirname } = require('path');
+const port = 8000;
 
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded());
 
-app.get('/',function(req,res)
-{
-   res.render('home',{title:"To Do List"});
-});
-
-app.get('/money',function(req,res)
-{
-  res.render('money',{title:"Manage Money"});
-});
-
-app.listen(port,function(err)
-{
-  if(err)
+var todolist = [
   {
-    console.log('Error in Connecting Server',err);
+    task: "Complete HomeWork",
+    type: "School",
+    date: "12/08/2001",
+    time: "12 Am"
+  },
+  {
+    task: "Complete HomeWork",
+    type: "School",
+    date: "12/08/2001",
+    time: "12 Am"
+  },
+  {
+    task: "Complete HomeWork",
+    type: "School",
+    date: "12/08/2001",
+    time: "12 Am"
+  },
+  {
+    task: "Complete HomeWork",
+    type: "School",
+    date: "12/08/2001",
+    time: "12 Am"
+  }
+];
+
+app.get('/', function (req, res) {
+  res.render('home', { title: "To Do List", todolist: todolist });
+});
+
+app.get('/money', function (req, res) {
+  res.render('money', { title: "Manage Money" });
+});
+
+app.post('/create_task',function(req,res)
+{
+   console.log(req.body)
+});
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log('Error in Connecting Server', err);
     return;
   }
-  else
-  {
-    console.log('Server is Up and Running on Port : ',port);
+  else {
+    console.log('Server is Up and Running on Port : ', port);
   }
 });
