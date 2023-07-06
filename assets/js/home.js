25202a0fb3c7cb6f5c67bbc70e39726d7cacb182
Akshay <98071270+AkKnight08@users.noreply.github.com>
2023-07-06 09:49:33 +0530

    Project Completed Now only more Designing is left

--- assets/js/home.js
+++ assets/js/home.js
@@ -4,6 +4,10 @@ function gettime() {
     var now = new Date();
     var hour = now.getHours();
     var min = now.getMinutes();
+    if (hour<=9)
+    hour='0'+hour;
+    if(min<=9)
+    min='0'+min;
     var time = hour + ":" + min;
     document.getElementById('dtime').innerHTML=time;
     var dd=now.getDay();
@@ -19,5 +23,26 @@ const btn=document.querySelectorAll('.click');
 btn.forEach((button)=>
 {
         const liElement = button.closest('li');
-        liElement.style.backgroundColor = 'yellow';
+        liElement.classList.add('checked');
 })
+const trimdate=document.querySelectorAll('#writebox2');
+for(let i of trimdate)
+{
+    let s="*"+i.innerHTML+"*";
+     s = s.replace(/\s/g, "");
+ s=s.slice(6,11)
+ i.innerHTML=s;
+}
+const p1= document.querySelectorAll('.Work');
+for (let i of p1) {
+    i.style.backgroundImage = "url('../images/work.jpeg')";
+}
+const p3 = document.querySelectorAll('.House');
+for (let i of p3) {
+    i.style.backgroundImage = "url('../images/house.jpeg')"
+}
+const p4 = document.querySelectorAll('.Others');
+for (let i of p4) {
+    i.style.backgroundImage = `url('../images/other.jpeg')`
+}
+
