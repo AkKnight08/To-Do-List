5a32fa3541389e6f3d4a32bfcacf39834c4b29d0
Akshay <98071270+AkKnight08@users.noreply.github.com>
2023-07-06 17:07:15 +0530

    MongoDb connected and now Readu for Deployment on Render.com

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/todolist', {
+mongoose.connect('mongodb+srv://Akshay:eL4oroP1AWcWNmSk@todolist.zd17u5y.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
